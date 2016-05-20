cfe7c28297268326478b5b873eca88d584e9d3a1
avikantz <avikantsainidbz@gmail.com>
2016-05-20 16:36:52 +0530

    Initial Commit

---
+++ Meme Maker Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Meme Maker Mac
+//
+//  Created by Avikant Saini on 5/20/16.
+//  Copyright © 2016 avikantz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
